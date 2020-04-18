e37b7edee4ba2bdfd4fe50ee210e791d36a84dd0
barakbeilin <barak.beilin@gmail.com>
2020-04-18 17:10:37 +0300

    donotmerge

--- sever.py
+++ sever.py
@@ -4,7 +4,7 @@ import socket
 
 HOST = '192.168.1.4'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-
+print("do not merge")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
